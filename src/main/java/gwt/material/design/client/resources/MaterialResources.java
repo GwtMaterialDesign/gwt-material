db8f729cf39a37fd907e82baa9584065f845d083
kevzlou7979 <kevzlou7979@gmail.com>
2015-04-15 17:00:46 +0800

    MAven Release

--- src/main/java/gwt/material/design/client/resources/MaterialResources.java
+++ src/main/java/gwt/material/design/client/resources/MaterialResources.java
@@ -3,14 +3,28 @@ package gwt.material.design.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.TextResource;
 
 public interface MaterialResources extends ClientBundle {
 	public static final MaterialResources INSTANCE = GWT
 			.create(MaterialResources.class);
 	
+	@Source("image.png")
 	ImageResource image();
+
+	@Source("ic_progress_cancel.png")
 	ImageResource ic_progress_cancel();
+
+	@Source("jquery.js")
+	TextResource jQuery();
+
+	@Source("materialize.min.js")
+	TextResource materializeJs();
+
+	@Source("materialcss.gss")
 	MaterialCSS materialcss();
+
+	@Source("materialmobilecss.gss")
 	MaterialMobileCSS materialmobilecss();
 
 }
--- src/main/java/gwt/material/design/client/resources/ResourcesLoader.java
+++ src/main/java/gwt/material/design/client/resources/ResourcesLoader.java
@@ -1,29 +1,54 @@
 package gwt.material.design.client.resources;
 
-import com.google.gwt.dom.client.StyleInjector;
-import com.google.gwt.resources.client.CssResource;
+/*
+ * #%L
+ * GwtMaterialDesign
+ * %%
+ * Copyright (C) 2015 GwtMaterial
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.google.gwt.core.client.ScriptInjector;
 
 public class ResourcesLoader {
-    private static final int MOBILE_MAX_WIDTH = 992;
-    private static final String MOBILE_MEDIA = "@media only screen and (max-width: " + MOBILE_MAX_WIDTH + "px), " +
-            "only screen and (max-device-width: " + MOBILE_MAX_WIDTH + "px) {";
-    private static final String CLOSING_BRACKET = "}";
 
     public ResourcesLoader(MaterialResources resources) {
-        resources.materialcss().ensureInjected();
-        injectMediaCss(resources);
+        injectJQuery(resources);
+        injectMaterializeJs(resources);
     }
- 
-    private void injectMediaCss(MaterialResources resources) {
-        injectMobileStylesheet(resources.materialmobilecss());
+
+    private void injectJQuery(MaterialResources resources) {
+        if (!isjQueryLoaded()) {
+            ScriptInjector.fromString(resources.jQuery().getText())
+                    .setWindow(ScriptInjector.TOP_WINDOW)
+                    .inject();
+        }
     }
- 
-    private void injectMobileStylesheet(CssResource cssResource) {
-        String mobileCss = wrapMobileStylesheet(cssResource.getText());
-        StyleInjector.injectAtEnd(mobileCss);
+
+    private void injectMaterializeJs(MaterialResources resources) {
+        ScriptInjector.fromString(resources.materializeJs().getText())
+                .setWindow(ScriptInjector.TOP_WINDOW)
+                .inject();
     }
- 
-    private String wrapMobileStylesheet(String content) {
-        return MOBILE_MEDIA + content + CLOSING_BRACKET;
-    }
+
+    /**
+     * Check to see if jQuery is loaded already
+     *
+     * @return true is jQuery is loaded, false otherwise
+     */
+    private native boolean isjQueryLoaded() /*-{
+        return (typeof $wnd['jQuery'] !== 'undefined');
+    }-*/;
 }
