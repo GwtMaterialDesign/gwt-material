381f5fd062f875669012de068154ef2d15c2cca0
wassx <wassx@gmx.net>
2015-06-22 21:28:47 +0200

    Renamed delegate method for materialdatepicker from "onCalendarClick" to "onCalendarClose"

--- src/main/java/gwt/material/design/client/ui/MaterialDatePicker.java
+++ src/main/java/gwt/material/design/client/ui/MaterialDatePicker.java
@@ -38,7 +38,7 @@ public class MaterialDatePicker extends FocusPanel{
 		 * Called as soon as a click occurs on the calendar widget. !EXPERIMENTAL!
 		 * @param currDate which is currently selected.
 		 */
-		void onCalendarClick(Date currDate);
+		void onCalendarClose(Date currDate);
 	}
 	
 	private HTMLPanel panel;
@@ -125,7 +125,7 @@ public class MaterialDatePicker extends FocusPanel{
 	
 	void notifyDelegate() {
 		if(delegate != null) {
-			delegate.onCalendarClick(getDate());
+			delegate.onCalendarClose(getDate());
 		}
 	}
 
