1e070743259fd2bd65377608d383e45fb9d33aa8
Maksym Ovsianikov <max32.pm@yandex.ru>
2015-07-07 18:36:43 -0700

    New implementation of MaterialCheckBoxCell based on AbstractEditableCell

--- src/main/java/gwt/material/design/client/custom/MaterialCheckBoxCell.java
+++ src/main/java/gwt/material/design/client/custom/MaterialCheckBoxCell.java
@@ -20,41 +20,124 @@ package gwt.material.design.client.custom;
  * #L%
  */
 
+import com.google.gwt.cell.client.AbstractEditableCell;
 import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.dom.client.BrowserEvents;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.InputElement;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 
-public class MaterialCheckBoxCell extends CheckboxCell {
+public class MaterialCheckBoxCell extends AbstractEditableCell<Boolean, Boolean> {
 
-	private static final SafeHtml INPUT_CHECKED = SafeHtmlUtils.fromSafeConstant("<input type=\"checkbox\" tabindex=\"-1\" checked/><label/>");
-	private static final SafeHtml INPUT_UNCHECKED = SafeHtmlUtils.fromSafeConstant("<input type=\"checkbox\" tabindex=\"-1\"/><label/>");
+    private static final SafeHtml INPUT_CHECKED = SafeHtmlUtils.fromSafeConstant("<input type=\"checkbox\" tabindex=\"-1\" checked/><label/>");
+    private static final SafeHtml INPUT_UNCHECKED = SafeHtmlUtils.fromSafeConstant("<input type=\"checkbox\" tabindex=\"-1\"/><label/>");
 
-	public MaterialCheckBoxCell() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    private final boolean dependsOnSelection;
+    private final boolean handlesSelection;
 
-	public MaterialCheckBoxCell(boolean dependsOnSelection, boolean handlesSelection) {
-		super(dependsOnSelection, handlesSelection);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Construct a new {@link CheckboxCell}.
+     */
+    public MaterialCheckBoxCell() {
+        this(false);
+    }
 
-	@Override
-	public void render(com.google.gwt.cell.client.Cell.Context context, Boolean value, SafeHtmlBuilder sb) {
-		Object key = context.getKey();
-		Boolean viewData = getViewData(key);
-		if (viewData != null && viewData.equals(value)) {
-			clearViewData(key);
-			viewData = null;
-		}
+    /**
+     * Construct a new {@link CheckboxCell} that optionally controls selection.
+     *
+     * @param isSelectBox
+     *            true if the cell controls the selection state
+     * @deprecated use {@link #CheckboxCell(boolean, boolean)} instead
+     */
+    @Deprecated
+    public MaterialCheckBoxCell(boolean isSelectBox) {
+        this(isSelectBox, isSelectBox);
+    }
 
-		if (value != null && ((viewData != null) ? viewData : value)) {
-			sb.append(INPUT_CHECKED);
-		}
-		else {
-			sb.append(INPUT_UNCHECKED);
-		}
-	}
+    /**
+     * Construct a new {@link CheckboxCell} that optionally controls selection.
+     *
+     * @param dependsOnSelection
+     *            true if the cell depends on the selection state
+     * @param handlesSelection
+     *            true if the cell modifies the selection state
+     */
+    public MaterialCheckBoxCell(boolean dependsOnSelection, boolean handlesSelection) {
+        super(BrowserEvents.CHANGE, BrowserEvents.KEYDOWN, BrowserEvents.CLICK);
+        this.dependsOnSelection = dependsOnSelection;
+        this.handlesSelection = handlesSelection;
+    }
 
+    @Override
+    public boolean dependsOnSelection() {
+        return dependsOnSelection;
+    }
+
+    @Override
+    public boolean handlesSelection() {
+        return handlesSelection;
+    }
+
+    @Override
+    public boolean isEditing(Context context, Element parent, Boolean value) {
+        // A checkbox is never in "edit mode". There is no intermediate state
+        // between checked and unchecked.
+        return false;
+    }
+
+    @Override
+    public void onBrowserEvent(Context context, Element parent, Boolean value, NativeEvent event, ValueUpdater<Boolean> valueUpdater) {
+        String type = event.getType();
+
+        boolean enterPressed = (BrowserEvents.KEYDOWN.equals(type) && event.getKeyCode() == KeyCodes.KEY_ENTER)
+                || event.getType().equals(BrowserEvents.CLICK);
+
+        if (BrowserEvents.CHANGE.equals(type) || enterPressed) {
+            InputElement input = parent.getFirstChild().cast();
+            Boolean isChecked = input.isChecked();
+
+            /*
+             * Toggle the value if the enter key was pressed and the cell handles selection or doesn't depend on selection. If the cell depends on selection but doesn't handle selection, then ignore
+             * the enter key and let the SelectionEventManager determine which keys will trigger a change.
+             */
+            if (enterPressed && (handlesSelection() || !dependsOnSelection())) {
+                isChecked = !isChecked;
+                input.setChecked(isChecked);
+            }
+
+            /*
+             * Save the new value. However, if the cell depends on the selection, then do not save the value because we can get into an inconsistent state.
+             */
+            if (value != isChecked && !dependsOnSelection()) {
+                setViewData(context.getKey(), isChecked);
+            } else {
+                clearViewData(context.getKey());
+            }
+
+            if (valueUpdater != null) {
+                valueUpdater.update(isChecked);
+            }
+        }
+    }
+
+    @Override
+    public void render(Context context, Boolean value, SafeHtmlBuilder sb) {
+        Object key = context.getKey();
+        Boolean viewData = getViewData(key);
+        if (viewData != null && viewData.equals(value)) {
+            clearViewData(key);
+            viewData = null;
+        }
+
+        if (value != null && ((viewData != null) ? viewData : value)) {
+            sb.append(INPUT_CHECKED);
+        } else {
+            sb.append(INPUT_UNCHECKED);
+        }
+    }
 }
