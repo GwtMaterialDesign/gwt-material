df4c82077e69d34ee6730424ad518fb7e71db618
Kevzlou7979 <kevzlou7979@gmail.com>
2015-11-14 00:42:55 +0800

    Updated the core libraries  Materializecss to 0.97.2

--- gwt-material/src/main/java/gwt/material/design/client/resources/MaterialDebugResources.java
+++ gwt-material/src/main/java/gwt/material/design/client/resources/MaterialDebugResources.java
@@ -30,6 +30,6 @@ public interface MaterialDebugResources extends ClientBundle {
     @Source("js/timepicker-0.1.0.js")
     TextResource timepickerJsDebug();
 
-    @Source("js/materialize-0.97.1.js")
+    @Source("js/materialize-0.97.2.js")
     TextResource materializeJsDebug();
 }
