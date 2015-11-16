a41ba99c85cacae88185162c832cd6f03f42e8b0
Kevzlou7979 <kevzlou7979@gmail.com>
2015-11-17 00:30:40 +0800

    Modal : Rewrite to have additional params based on http://materializecss.com/modals.html. Added Transitions, opacity, dismissable features.

---
+++ gwt-material/src/main/java/gwt/material/design/client/base/HasTransition.java
@@ -0,0 +1,17 @@
+package gwt.material.design.client.base;
+
+public interface HasTransition {
+
+    /**
+     * Sets the In Duration of transition
+     * @param inDuration
+     */
+    public void setInDuration(int inDuration);
+
+    /**
+     * Sets the our duration of transition
+     * @param outDuration
+     */
+    public void setOutDuration(int outDuration);
+
+}
