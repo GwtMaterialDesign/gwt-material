34e260b7b0f59893c0963414808641d745ed58ed
Kevzlou7979 <kevzlou7979@gmail.com>
2015-12-04 23:51:28 +0800

    Fixed : MaterialTab: indicatorColor is a raw color, not a color class.

--- gwt-material/src/main/java/gwt/material/design/client/ui/MaterialTab.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialTab.java
@@ -20,29 +20,24 @@ package gwt.material.design.client.ui;
  * #L%
  */
 
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 import gwt.material.design.client.base.ComplexWidget;
-import gwt.material.design.client.base.helper.ColorHelper;
+import gwt.material.design.client.base.mixin.ColorsMixin;
 import gwt.material.design.client.ui.html.UnorderedList;
 
 //@formatter:off
 
 /**
  * The tabs structure consists of an unordered list of tabs that have hashes corresponding to tab ids. Then when you click on each tab, only the container with the corresponding tab id will become visible.
-
  * <h3>UiBinder Usage:</h3>
  * <pre>
  *{@code
-
 <m:MaterialTab ui:field="tab"  backgroundColor="blue">
 <m:MaterialTabItem waves="YELLOW" grid="l4"><i:Link text="Tab 1" href="#tab1" textColor="white"/></m:MaterialTabItem>
 <m:MaterialTabItem waves="YELLOW" grid="l4"><i:Link text="Tab 2" href="#tab2" textColor="white"/></m:MaterialTabItem>
 <m:MaterialTabItem waves="YELLOW" grid="l4"><i:Link text="Tab 3" href="#tab3" textColor="white"/></m:MaterialTabItem>
 </m:MaterialTab>
-
 <i:Panel m:id="tab1">
 <i:Title title="Tab 1" description="Tab 1 Content"/>
 </i:Panel>
@@ -52,7 +47,6 @@ import gwt.material.design.client.ui.html.UnorderedList;
 <i:Panel m:id="tab3">
 <i:Title title="Tab 3" description="Tab 3 Content"/>
 </i:Panel>
-
 }
  * </pre>
  * @see <a href="http://gwt-material-demo.herokuapp.com/#tabs">Material Tabs</a>
@@ -63,7 +57,10 @@ import gwt.material.design.client.ui.html.UnorderedList;
 public class MaterialTab extends UnorderedList {
 
     private int tabIndex;
-    private String indicatorColor = "white";
+    private String indicatorColor;
+
+    private ComplexWidget indicator;
+    private ColorsMixin<ComplexWidget> indicatorColorMixin;
 
     public MaterialTab() {
         super();
@@ -74,6 +71,9 @@ public class MaterialTab extends UnorderedList {
     public void onLoad() {
         super.onLoad();
         initialize();
+        indicator = new ComplexWidget(getIndicatorElement(getElement()));
+        indicatorColorMixin = new ColorsMixin<>(indicator);
+        setIndicatorColor(indicatorColor);
     }
 
     public int getTabIndex() {
@@ -96,7 +96,10 @@ public class MaterialTab extends UnorderedList {
 
     public void setIndicatorColor(String indicatorColor){
         this.indicatorColor = indicatorColor;
-        applyIndicatorColor(getElement(), ColorHelper.setupComputedBackgroundColor(indicatorColor));
+
+        if(indicatorColorMixin != null && indicatorColor != null) {
+            indicatorColorMixin.setBackgroundColor(indicatorColor);
+        }
     }
 
     /**
@@ -109,7 +112,6 @@ public class MaterialTab extends UnorderedList {
 
     protected void initialize() {
         initialize(getElement());
-        applyIndicatorColor(getElement(), ColorHelper.setupComputedBackgroundColor(indicatorColor));
     }
 
     private native void initialize(Element e) /*-{
@@ -118,12 +120,8 @@ public class MaterialTab extends UnorderedList {
         });
     }-*/;
 
-    /**
-     * Line Indicator on Tab Navigation.
-     * @param color Color string
-     */
-    private native void applyIndicatorColor(Element e, String color)/*-{
-        $wnd.jQuery(e).find(".indicator").css("background-color", color);
+    private native Element getIndicatorElement(Element e)/*-{
+        return $wnd.jQuery(e).find(".indicator")[0];
     }-*/;
 
     private native void selectTab(Element e, String tabId)/*-{
