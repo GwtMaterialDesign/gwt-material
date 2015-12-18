ef9a76a98b8d78a476d687c1da3a684b7b9cf629
wassx <wassx@gmx.net>
2015-12-18 21:26:31 +0100

    Fixed instance handling of builder

--- gwt-material/src/main/java/gwt/material/design/client/ui/builder/MaterialModalBuilder.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/builder/MaterialModalBuilder.java
@@ -5,34 +5,68 @@ import gwt.material.design.client.constants.ModalType;
 import gwt.material.design.client.ui.MaterialModal;
 
 /**
- * Created by stefanwasserbauer on 29.11.15.
+ * Created by stefanwasserbauer (wassx) on 29.11.15.
  */
 public class MaterialModalBuilder {
 
 	private static MaterialModalBuilder INSTANCE;
 	private static MaterialModal materialModal;
 
+	/**
+	 * Starts the builder for creating a MaterialModal.
+	 * <br>For e.g. <code>MaterialModalBuilder.create().withWidget(new Label("Test")).isDismissable(true).build())</code>
+	 *
+	 * @return an instance of the builder.
+	 */
 	public static MaterialModalBuilder create() {
-		INSTANCE = new MaterialModalBuilder();
-		materialModal = new MaterialModal();
+		if (INSTANCE == null) {
+			INSTANCE = new MaterialModalBuilder();
+		}
+		if (materialModal == null) {
+			materialModal = new MaterialModal();
+		}
+		materialModal.clear();
 		return INSTANCE;
 	}
 
+	/**
+	 * Adds a widget to the MaterialModal
+	 *
+	 * @param widget e.g. Label
+	 * @return instance of builder
+	 */
 	public MaterialModalBuilder withWidget(Widget widget) {
 		materialModal.add(widget);
 		return INSTANCE;
 	}
 
+	/**
+	 * Sets appearance type of the MaterialModal
+	 *
+	 * @param type for appearance of modal
+	 * @return instance of builder
+	 */
 	public MaterialModalBuilder withType(ModalType type) {
 		materialModal.setType(type);
 		return INSTANCE;
 	}
 
-	public MaterialModalBuilder isDismissable(boolean b) {
-		materialModal.setDismissable(b);
+	/**
+	 * Sets if the MaterialModal is dismissible
+	 *
+	 * @param isDismissable true/false
+	 * @return instance of builder
+	 */
+	public MaterialModalBuilder isDismissable(boolean isDismissable) {
+		materialModal.setDismissable(isDismissable);
 		return INSTANCE;
 	}
 
+	/**
+	 * Finishes the builder flow and creates the MaterialModal
+	 *
+	 * @return instance of MaterialModal
+	 */
 	public MaterialModal build() {
 		return materialModal;
 	}
