b5487a4feefba195edd0f3d8edb44c2e8d048d03
Kevzlou7979 <kevzlou7979@gmail.com>
2016-01-05 15:25:21 +0800

    Added font weight property on material widget.

--- gwt-material/src/main/java/gwt/material/design/client/base/MaterialWidget.java
+++ gwt-material/src/main/java/gwt/material/design/client/base/MaterialWidget.java
@@ -59,7 +59,7 @@ import gwt.material.design.client.ui.animate.MaterialAnimator;
 
 public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, HasTextAlign, HasColors, HasGrid,
         HasShadow, Focusable, HasInlineStyle, HasSeparator, HasScrollspy, HasHideOn, HasShowOn, HasCenterOn,
-        HasCircle, HasWaves, HasDataAttributes, HasFloat, HasTooltip, HasFlexbox, HasHoverable {
+        HasCircle, HasWaves, HasDataAttributes, HasFloat, HasTooltip, HasFlexbox, HasHoverable, HasFontWeight {
 
     private IdMixin<MaterialWidget> idMixin;
     private EnabledMixin<MaterialWidget> enabledMixin;
@@ -80,6 +80,7 @@ public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, H
     private TooltipMixin<MaterialWidget> tooltipMixin;
     private FlexboxMixin<MaterialWidget> flexboxMixin;
     private ToggleStyleMixin<MaterialWidget> hoverableMixin;
+    private CssNameMixin<MaterialWidget, Style.FontWeight> fontWeightMixin;
 
     public MaterialWidget() {
     }
@@ -196,6 +197,11 @@ public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, H
     private FlexboxMixin<MaterialWidget> getFlexboxMixin() {
         if(flexboxMixin == null) { flexboxMixin = new FlexboxMixin<>(this); }
         return flexboxMixin;
+    }
+
+    private CssNameMixin<MaterialWidget, Style.FontWeight> getFontWeightMixin() {
+        if(fontWeightMixin == null) { fontWeightMixin = new CssNameMixin<>(this); }
+        return fontWeightMixin;
     }
 
     @Override
@@ -566,5 +572,15 @@ public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, H
     @Override
     public boolean isHoverable() {
         return getHoverableMixin().isOn();
+    }
+
+    @Override
+    public void setFontWeight(Style.FontWeight fontWeight) {
+        getFontWeightMixin().setCssName(fontWeight);
+    }
+
+    @Override
+    public Style.FontWeight getFontWeight() {
+        return getFontWeightMixin().getCssName();
     }
 }
