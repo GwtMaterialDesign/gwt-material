9b757241deab145d2f13c0c279ba75902bcf21a0
Kevzlou7979 <kevzlou7979@gmail.com>
2016-02-22 10:35:37 +0800

    Re-implemented the clear value on datepicker

--- gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDatePicker.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDatePicker.java
@@ -133,6 +133,7 @@ public class MaterialDatePicker extends MaterialWidget implements HasGrid, HasEr
             clearErrorOrSuccess();
             label.removeStyleName("active");
             dateInput.removeStyleName("valid");
+            dateInput.clear();
         }
     }
 
