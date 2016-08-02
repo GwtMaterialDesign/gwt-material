2904a1020822e0b878695108ead51fd88e01ef05
Kevzlou7979 <kevzlou7979@gmail.com>
2016-08-03 06:07:21 +0800

    Revert back the issue on $.wnd is not a  function.

--- gwt-material/src/main/java/gwt/material/design/client/base/MaterialWidget.java
+++ gwt-material/src/main/java/gwt/material/design/client/base/MaterialWidget.java
@@ -44,8 +44,8 @@ public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, H
         HasCircle, HasWaves, HasDataAttributes, HasFloat, HasTooltip, HasFlexbox, HasHoverable, HasFontWeight,
         HasDepth, HasInitialClasses {
 
-    public final static JQueryElement window = $(JQuery.window());
-    public final static JQueryElement body = $("body");
+    public final static JQueryElement window = window();
+    public final static JQueryElement body = body();
 
     /**
      * Configurable features enum see {@link #enableFeature(Feature, boolean)}.
@@ -114,6 +114,14 @@ public class MaterialWidget extends ComplexPanel implements HasId, HasEnabled, H
         if ($this == null) { $this = JQuery.$(this); }
         return $this;
     }
+
+    protected static native JQueryElement window() /*-{
+        return window;
+    }-*/;
+
+    protected static native JQueryElement body() /*-{
+        return document.getElementsByTagName("body")[0];
+    }-*/;
 
     @Override
     protected void onLoad() {
