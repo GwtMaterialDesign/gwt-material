ae779e72900134bb1750cc3375c81123dd0c5a36
Kevzlou7979 <kevzlou7979@gmail.com>
2016-08-18 13:27:00 +0800

    MaterialDropDown's setActivator doesn't function correctly #416

--- gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDropDown.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDropDown.java
@@ -200,6 +200,7 @@ public class MaterialDropDown extends UnorderedList implements HasSelectionHandl
      */
     public void setActivator(String activator) {
         this.activator = activator;
+        setId(activator);
     }
 
     @Override
