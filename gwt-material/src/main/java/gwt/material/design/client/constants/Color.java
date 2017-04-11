db9699da0c2b3d3c67427f48fa579e98cd5a9e8d
kevzlou7979 <kevzlou7979@gmail.com>
2017-04-11 20:53:35 +0800

    Fixed some typos in java docs.

--- gwt-material/src/main/java/gwt/material/design/client/constants/Color.java
+++ gwt-material/src/main/java/gwt/material/design/client/constants/Color.java
@@ -24,7 +24,7 @@ import gwt.material.design.client.base.helper.EnumHelper;
 /**
  * Color constants.
  *
- * @see <a href="http://127.0.0.1:8888/index.html#colors">Colors</a>
+ * @see <a href="http://gwtmaterialdesign.github.io/gwt-material-demo/#colors">Colors</a>
  * @see <a href="https://material.io/guidelines/style/color.html#">Material Design Specification</a>
  * @see <a href="https://material.io/color/">Color Tool</a>
  * @author kevzlou7979
