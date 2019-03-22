78ee499da9ab3a503b51670d8e76793afe69e8a1
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-03-22 09:09:36 +0800

    Minor cleanup

--- gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDropDown.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDropDown.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,9 +69,7 @@ import static gwt.material.design.client.js.JsMaterialElement.$;
 //@formatter:on
 public class MaterialDropDown<T> extends UnorderedList implements JsLoader, HasSelectionHandlers<Widget>, HasValueChangeHandlers<T>, HasInOutDurationTransition {
 
-    private T selected;
     private Map<Widget, T> widgetMap = new HashMap<>();
-    private DropdownItemRenderer<T> renderer;
     private String activator;
     private Element activatorElement;
     private List<HandlerRegistration> handlers = new ArrayList<>();
