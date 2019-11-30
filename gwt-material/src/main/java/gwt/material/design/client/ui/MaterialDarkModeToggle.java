90952c4f52ee82c43f4dfde824be14bb3f6dd05b
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-12-01 06:13:03 +0800

    MaterialDarkModeToggle added ColorSchemeChangeHandler

--- gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDarkModeToggle.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDarkModeToggle.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,12 +19,11 @@
  */
 package gwt.material.design.client.ui;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import gwt.material.design.client.constants.IconType;
-import gwt.material.design.client.theme.dark.ColorScheme;
-import gwt.material.design.client.theme.dark.DarkThemeManager;
-import gwt.material.design.client.theme.dark.HasDarkMode;
+import gwt.material.design.client.theme.dark.*;
 
-public class MaterialDarkModeToggle extends MaterialIcon implements HasDarkMode {
+public class MaterialDarkModeToggle extends MaterialIcon implements HasDarkMode, HasColorSchemeChangeHandler {
 
     protected DarkThemeManager darkThemeManager;
 
@@ -36,8 +35,10 @@ public class MaterialDarkModeToggle extends MaterialIcon implements HasDarkMode 
     protected void onLoad() {
         super.onLoad();
 
-
-        darkThemeManager.setCallback(colorScheme -> setIconType(colorScheme == ColorScheme.DARK ? IconType.BRIGHTNESS_7 : IconType.BRIGHTNESS_4));
+        darkThemeManager.setCallback(colorScheme -> {
+            setIconType(colorScheme == ColorScheme.DARK ? IconType.BRIGHTNESS_7 : IconType.BRIGHTNESS_4);
+            ColorSchemeChangeEvent.fire(this, colorScheme);
+        });
         darkThemeManager.load();
         registerHandler(addClickHandler(event -> setDarkMode(!isDarkMode())));
     }
@@ -57,5 +58,10 @@ public class MaterialDarkModeToggle extends MaterialIcon implements HasDarkMode 
     @Override
     public boolean isDarkMode() {
         return darkThemeManager.isDarkMode();
+    }
+
+    @Override
+    public HandlerRegistration addColorSchemeChangeHandler(ColorSchemeChangeEvent.ColorSchemeChangeHandler handler) {
+        return addHandler(handler, ColorSchemeChangeEvent.TYPE);
     }
 }
