2a84674f7b845540e580a73e9b68e56c87e8ba8e
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-12-04 15:48:33 +0800

    DarkThemeLoader - Children support

--- gwt-material/src/main/java/gwt/material/design/client/theme/dark/DarkThemeLoader.java
+++ gwt-material/src/main/java/gwt/material/design/client/theme/dark/DarkThemeLoader.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,7 @@ import com.google.gwt.resources.client.TextResource;
 
 public abstract class DarkThemeLoader {
 
+    private DarkThemeLoader[] children;
     protected StyleElement styleElement = null;
     protected TextResource resource;
 
@@ -32,13 +33,31 @@ public abstract class DarkThemeLoader {
         this.resource = resource;
     }
 
+    public DarkThemeLoader(DarkThemeLoader... children) {
+        this.children = children;
+    }
+
     public void load() {
-        styleElement = StyleInjector.injectStylesheet(resource.getText());
+        if (children != null) {
+            for (DarkThemeLoader child : children) {
+                if (child != null) {
+                    child.load();
+                }
+            }
+        } else {
+            styleElement = StyleInjector.injectStylesheet(resource.getText());
+        }
     }
 
     public void unload() {
-        if (styleElement != null) {
-            styleElement.removeFromParent();
+        if (children != null) {
+            for (DarkThemeLoader loader : children) {
+                loader.unload();
+            }
+        } else {
+            if (styleElement != null) {
+                styleElement.removeFromParent();
+            }
         }
     }
 }
