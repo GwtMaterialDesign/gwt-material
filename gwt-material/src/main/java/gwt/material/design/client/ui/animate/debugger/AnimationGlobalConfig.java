977897ba6062e2b3bd3dbdd87e1d8685d07798ec
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-03-28 15:56:44 +0800

    AnimationSpeed defaults to Normal

--- gwt-material/src/main/java/gwt/material/design/client/ui/animate/debugger/AnimationGlobalConfig.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/animate/debugger/AnimationGlobalConfig.java
@@ -2,6 +2,6 @@ package gwt.material.design.client.ui.animate.debugger;
 
 public class AnimationGlobalConfig {
 
-    public static AnimationSpeed SPEED = AnimationSpeed.SLOWEST;
+    public static AnimationSpeed SPEED = AnimationSpeed.NORMAL;
     public static Boolean ENABLE_DEBUGGING = true;
 }
