cced73f2ab66b3d305b163e4044e3ea028ac3fbe
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-04-20 15:00:22 +0800

    AnimationGlobalConfig - turn off debugging by default.

--- gwt-material/src/main/java/gwt/material/design/client/ui/animate/debugger/AnimationGlobalConfig.java
+++ gwt-material/src/main/java/gwt/material/design/client/ui/animate/debugger/AnimationGlobalConfig.java
@@ -22,6 +22,6 @@ package gwt.material.design.client.ui.animate.debugger;
 public class AnimationGlobalConfig {
 
     public static AnimationSpeed SPEED = AnimationSpeed.NORMAL;
-    public static Boolean ENABLE_DEBUGGING = true;
+    public static Boolean ENABLE_DEBUGGING = false;
     public static Boolean ENABLE_ANIMATION = true;
 }
