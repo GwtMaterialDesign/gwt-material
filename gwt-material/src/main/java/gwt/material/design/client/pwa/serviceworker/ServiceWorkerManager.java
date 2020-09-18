64f767d415ca59a219c5d4f046e79a8ff29e6b80
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-09-19 06:26:48 +0800

    Fixed https://github.com/GwtMaterialDesign/gwt-material/issues/986

--- gwt-material/src/main/java/gwt/material/design/client/pwa/serviceworker/ServiceWorkerManager.java
+++ gwt-material/src/main/java/gwt/material/design/client/pwa/serviceworker/ServiceWorkerManager.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -105,7 +105,7 @@ public class ServiceWorkerManager implements ServiceWorkerLifecycle, PwaFeature 
      */
     protected void setupRegistration() {
         if (isServiceWorkerSupported()) {
-            Navigator.serviceWorker.register(getResource(), getOption()).then((e, object) -> {
+            Navigator.serviceWorker.register(getResource(), getOption()).then((object) -> {
                 logger.info("Service worker has been successfully registered");
                 registration = (ServiceWorkerRegistration) object;
 
