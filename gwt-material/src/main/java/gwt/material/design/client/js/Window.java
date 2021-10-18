c0963587b653dc4dad00673688872a105d86b288
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2021-10-19 07:06:37 +0800

    Added native atob and unescape on Window Object.

--- gwt-material/src/main/java/gwt/material/design/client/js/Window.java
+++ gwt-material/src/main/java/gwt/material/design/client/js/Window.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,9 +38,15 @@ public class Window {
     public static native MediaQueryList getMediaQueryList(String query) /*-{
         return $wnd.window.matchMedia(query);
     }-*/;
-
+    
     @JsMethod(namespace = "window")
     public static native void addEventListener(String eventName, Functions.Func1<Object> event);
+
+    @JsMethod(namespace = "window")
+    public static native String atob(String param);
+
+    @JsMethod(namespace = "window")
+    public static native String unescape(String param);
 
     @JsMethod
     public static native DeferredPrompt deferredPrompt();
