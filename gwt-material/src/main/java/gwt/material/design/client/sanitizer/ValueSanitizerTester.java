75557fbe0641136da1810adbfeeacf0616c47021
jeremy.lin1 <jeremy.lin@insclix.com>
2022-09-21 13:47:30 +0800

    Added additional reserved keywords

--- gwt-material/src/main/java/gwt/material/design/client/sanitizer/ValueSanitizerTester.java
+++ gwt-material/src/main/java/gwt/material/design/client/sanitizer/ValueSanitizerTester.java
@@ -6,7 +6,7 @@ public class ValueSanitizerTester {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Valid : " + sanitizer.sanitize("$"));
+            System.out.println("Valid : " + sanitizer.sanitize("NIL"));
         } catch (Exception e) {
             System.out.println(e + "");
         }
