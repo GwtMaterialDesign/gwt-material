23c143854e78c017a9b4e8969810b097257aed3e
kevzlou7979 <kevzlou7979@gmail.com>
2023-03-24 10:07:17 +0800

    Introduction to GMD Theme Config.

---
+++ gwt-material/src/main/java/gwt/material/design/client/theme/GlobalThemeConfig.java
@@ -0,0 +1,16 @@
+package gwt.material.design.client.theme;
+
+import gwt.material.design.client.constants.IconDisplay;
+
+public class GlobalThemeConfig {
+
+    protected IconDisplay iconDisplay = IconDisplay.FILLED;
+
+    public IconDisplay getIconDisplay() {
+        return iconDisplay;
+    }
+
+    public void setIconDisplay(IconDisplay iconDisplay) {
+        this.iconDisplay = iconDisplay;
+    }
+}
